5550ede3af19971172c75c76bf9f22fee9990e0d
Derek <48116904+SoLetsCode@users.noreply.github.com>
2021-05-07 12:00:02 -0700

    added movie cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import { Button, Container, TextField } from "@material-ui/core";
+import MovieCard from "./components/MovieCard";
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+
   const API_KEY =
     process.env.NODE_ENV === "development"
       ? process.env.REACT_APP_API_KEY
       : process.env.REACT_APP_GH_API_KEY;
   const URL = "https://www.omdbapi.com";
-  const PARAMS = `?apikey=${API_KEY}&type=movie&s=`;
+  const PARAMS = `?apikey=${API_KEY}&type=movie`;
 
   const searchSubmit = (e) => {
     e.preventDefault();
     const search = e.target.title.value;
     axios
-      .get(`${URL}${PARAMS}${search}`)
+      .get(`${URL}${PARAMS}&s=${search}`)
       .then((res) => {
         if (res.data.Response === "False") {
           //no movie found
@@ -23,8 +26,11 @@ export default function App() {
         }
 
         if (res.data?.Search) {
+          setMovies(res.data.Search);
           console.log(res.data.Search);
         }
+
+        e.target.title.value = "";
       })
       .catch((error) => {
         console.log(error);
@@ -49,6 +55,20 @@ export default function App() {
         >
           Search
         </Button>
+      </Container>
+      {/* Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTQ5MjYxMjkwOV5BMl5BanBnXkFtZTgwODE3MjY0MzE@._V1_SX300.jpg"
+      Title: "Hello Ladies: The Movie" Type: "movie" Year: "2014" imdbID:
+      "tt3762944" */}
+      <Container>
+        {movies.map((movie) => (
+          <MovieCard title={movie.Title} year={movie.Year} src={movie.Poster} />
+        ))}
+        {/* <MovieCard
+          title="Hello Ladies: The Movie"
+          year="2014"
+          src="https://m.media-amazon.com/images/M/MV5BMTQ5MjYxMjkwOV5BMl5BanBnXkFtZTgwODE3MjY0MzE@._V1_SX300.jpg"
+        /> */}
       </Container>
     </div>
   );
