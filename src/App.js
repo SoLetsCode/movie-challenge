5eee3db378b7113d1e1ce817ac23c9ce3b42df3e
Derek <48116904+SoLetsCode@users.noreply.github.com>
2021-05-06 18:15:03 -0700

    trying to obfuscate API key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import axios from "axios";
+
 import { Button, Container, TextField } from "@material-ui/core";
 
+console.log(process.env.REACT_APP_API_KEY);
+console.log(process.env.NODE_ENV);
 export default function App() {
+  const API_KEY =
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_API_KEY
+      : process.env.REACT_APP_API_KEY;
+  const URL = "http://www.omdbapi.com";
+  const PARAMS = `?apikey=${API_KEY}&type=movie&s=`;
+
   const searchClick = (e) => {
     e.preventDefault();
-    console.log(e.target.title.value);
+    const search = e.target.title.value;
+    axios.get(`${URL}${PARAMS}${search}`).then((res) => {
+      console.log(res);
+    });
   };
   return (
     <div>
