4ab3c27a51a54cb645bf19db8d815b84be6c6513
Derek <48116904+SoLetsCode@users.noreply.github.com>
2021-05-07 17:37:16 -0700

    created search bar component and tidied up search results display

--- src/App.js
+++ src/App.js
@@ -1,74 +1,27 @@
 import React, { useState } from "react";
-import axios from "axios";
 
-import { Button, Container, TextField } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import MovieCard from "./components/MovieCard";
+import SearchBar from "./components/SearchBar";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [nominated, setNominated] = useState([]);
+  const [search, setSearch] = useState("Please search for movies");
 
-  const API_KEY =
-    process.env.NODE_ENV === "development"
-      ? process.env.REACT_APP_API_KEY
-      : process.env.REACT_APP_GH_API_KEY;
-  const URL = "https://www.omdbapi.com";
-  const PARAMS = `?apikey=${API_KEY}&type=movie`;
-
-  const searchSubmit = (e) => {
-    e.preventDefault();
-    const search = e.target.title.value;
-    axios
-      .get(`${URL}${PARAMS}&s=${search}`)
-      .then((res) => {
-        if (res.data.Response === "False") {
-          //no movie found
-          console.log("MOVIE NOT FOUND");
-        }
-
-        if (res.data?.Search) {
-          setMovies(res.data.Search);
-          console.log(res.data.Search);
-        }
-
-        e.target.title.value = "";
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
   return (
     <div>
+      <SearchBar setMovies={setMovies} setSearch={setSearch} />
       <Container>
-        <form
-          noValidate
-          autoComplete="off"
-          id="searchForm"
-          onSubmit={searchSubmit}
-        >
-          <TextField label="Search" name="title" variant="outlined" />
-        </form>
-        <Button
-          type="submit"
-          variant="contained"
-          form="searchForm"
-          color="primary"
-        >
-          Search
-        </Button>
-      </Container>
-      {/* Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTQ5MjYxMjkwOV5BMl5BanBnXkFtZTgwODE3MjY0MzE@._V1_SX300.jpg"
-      Title: "Hello Ladies: The Movie" Type: "movie" Year: "2014" imdbID:
-      "tt3762944" */}
-      <Container>
+        {search}
         {movies.map((movie) => (
           <MovieCard title={movie.Title} year={movie.Year} src={movie.Poster} />
         ))}
-        {/* <MovieCard
-          title="Hello Ladies: The Movie"
-          year="2014"
-          src="https://m.media-amazon.com/images/M/MV5BMTQ5MjYxMjkwOV5BMl5BanBnXkFtZTgwODE3MjY0MzE@._V1_SX300.jpg"
-        /> */}
+      </Container>
+      <Container>
+        {nominated.map((movie) => (
+          <MovieCard title={movie.Title} year={movie.Year} src={movie.Poster} />
+        ))}
       </Container>
     </div>
   );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+
+import { Button, Container, TextField } from "@material-ui/core";
+
+function SearchBar({ setMovies, setSearch }) {
+  const API_KEY =
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_API_KEY
+      : process.env.REACT_APP_GH_API_KEY;
+  const URL = "https://www.omdbapi.com";
+  const PARAMS = `?apikey=${API_KEY}&type=movie`;
+
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    const search = e.target.title.value;
+    axios
+      .get(`${URL}${PARAMS}&s=${search}`)
+      .then((res) => {
+        if (res.data.Response === "False") {
+          //no movie found
+          setSearch(`No movies found that contain "${e.target.title.value}"`);
+          setMovies([]);
+        }
+
+        if (res.data?.Search) {
+          setSearch(`Results for "${e.target.title.value}"`);
+          setMovies(res.data.Search);
+          console.log(res.data.Search);
+        }
+
+        e.target.title.value = "";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Container>
+      <form
+        noValidate
+        autoComplete="off"
+        id="searchForm"
+        onSubmit={searchSubmit}
+      >
+        <TextField label="Search" name="title" variant="outlined" />
+      </form>
+      <Button
+        type="submit"
+        variant="contained"
+        form="searchForm"
+        color="primary"
+      >
+        Search
+      </Button>
+    </Container>
+  );
+}
+
+export default SearchBar;
